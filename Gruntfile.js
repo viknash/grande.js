74c760af95e171791e5b4486d1e9f24050ba2463
Viknash <viknash@hobbiz.com>
2015-04-19 14:11:51 +0800

    Added travis lint\

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,30 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  grunt.initConfig({
-    qunit: {
-      options: {
-        "--web-security": "no",
-        coverage: {
-          src: ["js/grande.js"],
-          instrumentedFiles: "temp/",
-          htmlReport: "report/coverage",
-          coberturaReport: "report/"
+    grunt.initConfig({
+        qunit: {
+            options: {
+                "--web-security": "no",
+                coverage: {
+                    src: ["js/grande.js"],
+                    instrumentedFiles: "temp/",
+                    htmlReport: "report/coverage",
+                    coberturaReport: "report/"
+                }
+            },
+            all: ["test/**.html"]
+        },
+        uglify: {
+            all: {
+                files: {
+                    'dist/grande.min.js': ['js/grande.js']
+                }
+            }
         }
-      },
-      all: ["test/**.html"]
-    },
-    uglify: {
-      all: {
-        files: {
-          'dist/grande.min.js': ['js/grande.js']
-        }
-      }
-    }
-  });
+    });
 
-  grunt.loadNpmTasks("grunt-qunit-istanbul");
-  grunt.loadNpmTasks("grunt-contrib-uglify");
-  // @TODO: add lint hook here as well for eslint
-  grunt.registerTask("travis", "qunit");
+    grunt.loadNpmTasks("grunt-qunit-istanbul");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    // @TODO: add lint hook here as well for eslint
+    grunt.loadNpmTasks('grunt-travis-lint');
+    grunt.registerTask("travis", ["travis-lint", "qunit"]);
 };
-
